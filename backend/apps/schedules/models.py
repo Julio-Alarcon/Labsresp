0cdcc830e446a31874d3903c137c3ff849eba723
Julio Alarcon Caballero <tazkech@gmail.com>
2021-10-21 00:09:45 -0300

    Modulo sistema de peticiones

--- backend/apps/schedules/models.py
+++ backend/apps/schedules/models.py
@@ -1,29 +1,42 @@
+import django
 from django.db import models
 from django.contrib.auth.models import User as auth_user
 from django.db.models.base import Model
-from apps.core.models import Workstation, Room
-import datetime
+from django.utils import timezone
+from apps.core.models import Workstation, Room, Campus
+from datetime import datetime, timedelta
 import os
 
 # Create your models here.
 
 class Petition(models.Model):
     licenses = [
-        (0,"")
+        (1,"EJ"), (2,"EJ2")
     ]
 
     name_petition = models.CharField(max_length=100, default="")
     email_petition = models.CharField(max_length=100, default="")
-    nrc_petition = models.CharField(max_length=10, default="")
-    laboratory_petition = models.CharField(max_length=10, default="")
+    nrc_petition = models.CharField(max_length=10, default="", blank=True)
+    campus_petition = models.ForeignKey(Campus, on_delete=models.SET_NULL,null=True)
+    laboratory_petition = models.ForeignKey(Room, on_delete=models.SET_NULL,null=True,blank=True)
     pc_petition = models.CharField(max_length=3, default="")
-    time_start = models.DateTimeField(null=True)
-    time_finish = models.DateTimeField(null=True)
-    license = models.CharField(max_length=50, default="", choices=licenses)
-    memo = models.CharField(max_length=250, default="")
-    status = models.CharField(max_length=20, default="")
+    day_start_petition = models.DateField(null=True, default=timezone.now())
+    time_start_petition = models.TimeField(null=True, default=timezone.now())
+    day_finish_petition = models.DateField(null=True, default=timezone.now())
+    time_finish_petition = models.TimeField(null=True, default=timezone.now())
+    day_petition = models.CharField(max_length=20, default="")
+    license_petition = models.CharField(max_length=20, choices=licenses, blank=True)
+    memo_petition = models.CharField(max_length=250, default="")
+    status_petition = models.CharField(max_length=20, default="Pendiente", blank=True)
+    active_petition = models.BooleanField(default=False)
     def __str__(self):
+        return "{}".format(self.name_petition)
+    def __unicode__(self):
         return "{}".format(self.name_petition)
 
 class Schedule(models.Model):
-    workstation=models.ForeignKey(Workstation,on_delete=models.SET_NULL,null=True,blank=True)
+    petition = models.ForeignKey(Petition, on_delete=models.SET_NULL,null=True, blank=True)
+    def __str__(self):
+        return "{}".format(self.day)
+    def __unicode__(self):
+        return "{}".format(self.day)
